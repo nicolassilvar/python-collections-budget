6114b2c7730001f612513794f06088d76b95b61e
nicolassilvar <nicolas.silva.r@gmail.com>
2020-06-29 20:57:26 -0500

    Update to Please

--- budget/FrequentExpenses.py
+++ budget/FrequentExpenses.py
@@ -1,9 +1,9 @@
-import Expense
+from . import Expense
 import collections
 import matplotlib.pyplot as plt
 
 expenses = Expense.Expenses()
-expenses.read_expenses('../data/spending_data.csv')
+expenses.read_expenses('data/spending_data.csv')
 spending_categories = []
 
 for expense in expenses.list:
